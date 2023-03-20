ada7dac0fdecac5e3933ae34522ad4887efd939f
D.Koryakovsky <d.koryakovsky@oceanpribor.ru>
2023-03-20 16:20:49 +0300

    some changes

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from os import listdir
 
 def getSound(name, textPages):
     sound = gtts.gTTS(textPages)
-    sound.save(name)
+    fullName = name + '.mp3'
+    sound.save(fullName)
 
 def getText(fileName):
     if '.pdf' in fileName:
@@ -24,20 +25,25 @@ def getText(fileName):
         textDocx = textract.process(fileName)
         text = str(textDocx).replace('\n', '').replace('\xc2', '').replace('\xa0', '')
         print(text)
+    elif '.txt' in fileName:
+        # txt files
+        reader = open(fileName, 'r')
+        text = str(reader.readlines())
+        reader.close()
     return text
-
 
 
 pathWithFiles = 'E:\\pythonFiles\\'
 start = time.perf_counter()
 
+
+# all files in path
 for file in os.listdir(pathWithFiles):
     fullPath = pathWithFiles + file
     print(fullPath)
-    getSound(fullPath, getText(fullPath))
+    getSound(file.split('.')[0], getText(fullPath))
 
 end = time.perf_counter() - start
-
 print(f"The time of execution of above program is: {end:0.4f} ms")
 
 
